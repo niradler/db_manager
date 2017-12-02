7064fd00768e3eb11d197d1568c1a0edd9b421aa
MOONSITE-AD\nira <nira@moonsite.com>
2017-12-02 15:27:33 +0200

    added store

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,21 @@
 import webSqlApi from './webSqlApi';
 import pouchdbApi from './pouchdbApi';
-export {
-    webSqlApi,
-    pouchdbApi
+class ConStore {
+    constructor() {
+        this.conArr = [];
+    }
+    addConnections(con) {
+        this.conArr.push(con)
+    }
+    updateConnections(con,i) {
+        this.conArr[i] = con;
+    }
+    getConnections() {
+return this. conArr;
+    }
+    delConnections(uid) {
+
+    }
 }
+const store = new ConStore();
+export default store;
